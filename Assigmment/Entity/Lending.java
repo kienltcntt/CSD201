cf65ae409cf2c7ab235a618b0849d5907feecc0e
KienLT <68058309+kienltcntt@users.noreply.github.com>
2020-12-10 10:14:04 +0700

    Create Lending.java

---
+++ Assigmment/Entity/Lending.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+public class Lending {
+    private String bcode;
+    private String rcode;
+    private int state;
+}
